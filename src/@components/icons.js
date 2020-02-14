714153a1556f8d03a5072236efc42ff70762a076
Neko250 <carlos.aguilar.112358@gmail.com>
2020-02-14 22:18:09 +0100

    refactor: icons

---
+++ src/@components/icons.js
@@ -0,0 +1,11 @@
+export {
+  BookmarkRounded as StoryIcon,
+  BusinessRounded as JobsIcon,
+  HomeRounded as HomeIcon,
+  ImportContactsRounded as StoriesIcon,
+  InfoRounded as InfoIcon,
+  MenuRounded as MenuIcon,
+  ModeCommentRounded as CommentIcon,
+  SubdirectoryArrowRightRounded as SectionIcon,
+  WorkRounded as JobIcon
+} from '@material-ui/icons'
--- src/components/icons.js
+++
@@ -1,9 +0,0 @@
-export {
-  BookmarkRounded as StoryIcon,
-  BusinessRounded as JobsIcon,
-  HomeRounded as HomeIcon,
-  ImportContactsRounded as StoriesIcon,
-  InfoRounded as InfoIcon,
-  ModeCommentRounded as CommentIcon,
-  WorkRounded as JobIcon
-} from '@material-ui/icons'
