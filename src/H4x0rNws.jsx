114d81397cd12a9a184f511a99b4ef569dbe33cb
Neko250 <carlos.aguilar.112358@gmail.com>
2020-02-15 01:24:34 +0100

    feat: extract app component

---
+++ src/H4x0rNws.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { ThemeProvider } from '@material-ui/styles'
+import { CssBaseline } from '@material-ui/core'
+import 'typeface-ibm-plex-mono'
+import 'typeface-roboto'
+
+import theme from '@themes/dark'
+import store from '@store'
+import { Navbar, Tabs } from '@components'
+import { Home, Stories, Jobs, About, NotFound } from '@views'
+import './index.css'
+
+export const asset = (path = '') => process.env.REACT_APP_BASENAME + path
+
+const H4x0rNws = () => (
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter basename={asset()}>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/stories' component={Stories} />
+          <Route path='/jobs' component={Jobs} />
+          <Route path='/about' component={About} />
+          <Route component={NotFound} />
+        </Switch>
+        <Route path={['/stories', '/jobs']} component={Tabs} />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>
+)
+
+export default H4x0rNws
--- src/index.jsx
+++ src/index.jsx
@@ -1,39 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { ThemeProvider } from '@material-ui/styles'
-import { CssBaseline } from '@material-ui/core'
-import 'typeface-ibm-plex-mono'
-import 'typeface-roboto'
-
-import theme from '@themes/dark'
-import store from '@store'
-import { Navbar, Tabs } from '@components'
-import { Home, Stories, Jobs, About, NotFound } from '@views'
+import { render } from 'react-dom'
+import H4x0rNws from 'H4x0rNws'
 import * as serviceWorker from 'serviceWorker'
-import './index.css'
 
-const H4x0rNws = () => {
-  return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <BrowserRouter basename='/H4X0R.nws'>
-          <Navbar />
-          <Switch>
-            <Route path='/' exact component={Home} />
-            <Route path='/stories' component={Stories} />
-            <Route path='/jobs' component={Jobs} />
-            <Route path='/about' component={About} />
-            <Route component={NotFound} />
-          </Switch>
-          <Route path={['/stories', '/jobs']} component={Tabs} />
-        </BrowserRouter>
-      </ThemeProvider>
-    </Provider>
-  )
-}
-
-ReactDOM.render(<H4x0rNws />, document.querySelector('#l33t'))
+render(<H4x0rNws />, document.querySelector('#l33t'))
 serviceWorker.register()
