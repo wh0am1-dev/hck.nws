25b153d7c19d8888cbd66c8a2f2f69ab0b152de4
Neko250 <carlos.aguilar.112358@gmail.com>
2020-09-23 19:07:53 +0200

    style: remove ogcard hover elevation

--- src/@components/OGCard/OGCard.jsx
+++ src/@components/OGCard/OGCard.jsx
@@ -1,6 +1,5 @@
-import React, { useCallback, useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import clsx from 'clsx'
 import {
   Button,
   Card,
@@ -10,46 +9,34 @@ import {
   CardActions,
   Typography
 } from '@material-ui/core'
-import useStyles from './OGCard.styles'
 
-const OGCard = ({ author, date, description, image, logo, publisher, title, url, video }) => {
-  const classes = useStyles()
-  const [elevate, setElevate] = useState(false)
-  const viewStory = useCallback(() => window.open(url, '_blank'), [url])
-
-  return (
-    <Card
-      elevation={elevate ? 4 : 0}
-      className={clsx(classes.card, elevate && classes.cardHover)}
-      onMouseEnter={() => setElevate(true)}
-      onMouseLeave={() => setElevate(false)}
-      onTouchStart={() => setElevate(true)}
-      onTouchEnd={() => setElevate(false)}
-    >
-      {(image || logo) && (
-        <CardActionArea onClick={viewStory}>
-          <CardMedia component='img' src={image || logo} />
-        </CardActionArea>
-      )}
-      <CardContent>
-        <Typography variant='h5' gutterBottom>
-          {title}
+const OGCard = ({ author, date, description, image, logo, publisher, title, url, video }) => (
+  <Card elevation={4}>
+    {(image || logo) && (
+      <CardActionArea onClick={() => window.open(url, '_blank')}>
+        <CardMedia component='img' src={image || logo} />
+      </CardActionArea>
+    )}
+    <CardContent>
+      <Typography variant='h5' gutterBottom>
+        {title}
+      </Typography>
+      {(author || date) && (
+        <Typography variant='subtitle1' component='span' gutterBottom>
+          {author} {date && `@ ${date?.split('T').slice(0, 1)}`}
         </Typography>
-        {(author || date) && (
-          <Typography variant='subtitle1' component='span' gutterBottom>
-            {author} {date && `@ ${date?.split('T').slice(0, 1)}`}
-          </Typography>
-        )}
-        <Typography variant='body1'>{description}</Typography>
-      </CardContent>
-      <CardActions>
-        <Button color='primary' onClick={viewStory}>
-          view story
-        </Button>
-      </CardActions>
-    </Card>
-  )
-}
+      )}
+      <Typography variant='body1' gutterBottom>
+        {description}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button color='primary' onClick={() => window.open(url, '_blank')}>
+        view story
+      </Button>
+    </CardActions>
+  </Card>
+)
 
 OGCard.propTypes = {
   author: PropTypes.string,
