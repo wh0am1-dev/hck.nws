808d708c47ab9a0248bbedc301f733e621726e37
Neko250 <hi@wh0am1.dev>
2020-10-09 00:21:37 +0200

    feat: add open-graph tags

--- src/@app/H4x0rNws.jsx
+++ src/@app/H4x0rNws.jsx
@@ -15,7 +15,16 @@ import './index.css'
 
 const H4x0rNws = () => (
   <Provider store={store}>
-    <Helmet defaultTitle='H4X0R.nws' titleTemplate='H4X0R.nws · %s' />
+    <Helmet defaultTitle='H4X0R.nws' titleTemplate='H4X0R.nws · %s'>
+      <meta property='og:title' content='H4X0R.nws' />
+      <meta property='og:description' content='hacker news prog web app' />
+      <meta property='og:type' content='website' />
+      <meta property='og:url' content='https://wh0am1.dev/H4X0R.nws' />
+      <meta
+        property='og:image'
+        content='https://raw.githubusercontent.com/Neko250/H4X0R.nws/master/public/img/h4x0r.nws.512.png'
+      />
+    </Helmet>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Snacks />
