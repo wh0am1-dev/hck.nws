3e5060d589f116ee01b0c7664173a6f7cec02776
Carlos Aguilar <hi@wh0am1.dev>
2022-07-26 15:41:40 +0200

    fix: service worker fetch

--- src/sw.js
+++ src/sw.js
@@ -1,13 +1,24 @@
 import { manifest, version } from '@parcel/service-worker'
 
-async function install() {
+addEventListener('install', e => e.waitUntil(install()))
+addEventListener('activate', e => e.waitUntil(activate()))
+addEventListener('fetch', e => e.respondWith(cacheFirst(e.request)))
+
+const install = async () => {
   const cache = await caches.open(version)
   await cache.addAll(manifest)
 }
-addEventListener('install', e => e.waitUntil(install()))
 
-async function activate() {
+const activate = async () => {
   const keys = await caches.keys()
   await Promise.all(keys.map(key => key !== version && caches.delete(key)))
 }
-addEventListener('activate', e => e.waitUntil(activate()))
+
+const cacheFirst = async request => {
+  const cached = await caches.match(request)
+  if (cached) return cached
+  const response = await fetch(request)
+  const cache = await caches.open(version)
+  await cache.put(request, response.clone())
+  return response
+}
