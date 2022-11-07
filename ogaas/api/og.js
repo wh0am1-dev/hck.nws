6b88f2fda91fecccdccc92104df3749122080b68
Carlos Aguilar <hi@wh0am1.dev>
2022-11-07 01:09:55 +0100

    feat: add ogaas pkg

---
+++ ogaas/api/og.js
@@ -0,0 +1,43 @@
+const got = require('got')
+const metascraper = require('metascraper')([
+  require('metascraper-author')(),
+  require('metascraper-date')(),
+  require('metascraper-description')(),
+  require('metascraper-image')(),
+  require('metascraper-logo')(),
+  require('metascraper-publisher')(),
+  require('metascraper-title')(),
+  require('metascraper-url')(),
+  require('metascraper-video')()
+])
+
+const handler = async (req, res) => {
+  const target = req.body && req.body.url
+  if (!target) res.status(418).json({})
+
+  const { body: html, url } = await got(target)
+  const metadata = await metascraper({ html, url })
+  res.status(200).json(metadata)
+}
+
+const allowCORS = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+
+  return await fn(req, res)
+}
+
+module.exports = allowCORS(handler)
